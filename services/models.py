5bd95b833438adeb780e29b99e3025abd0158280
Fabian Montero <fabian@cluster451.org>
2022-01-27 22:14:38 -0600

    Añade comentarios a services.

--- services/models.py
+++ services/models.py
@@ -2,6 +2,26 @@ from django.db.models import Model, TextField, URLField
 
 
 class Service(Model):
+    """Representa un algún servicio que la institución brinda.
+
+    Por ejemplo:
+        - Psicología
+        - Clínica
+        - Impresión en 3D
+        - Fotocipiado
+
+    name:
+    Nombre del servicio.
+
+    description:
+    Breve descripción del servicio.
+
+    link:
+    Url del servicio en la página del Tec.
+
+    contact:
+    Información de contacto para usar el servicio.
+    """
     name = TextField(max_length=500, unique=True)
     description = TextField(max_length=5000)
     link = URLField(max_length=500)
