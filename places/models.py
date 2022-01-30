24645bc8f524c493d0d0351255d4a1f2e2a13574
Alejandro Díaz <alejandrodp@protonmail.com>
2022-01-30 12:55:06 -0600

    Añade esquema de BD para contribuciones a módulo places
    Añade la respresentación de un editor, una sugerencia de edición y de agregado de un lugar

--- places/models.py
+++ places/models.py
@@ -1,5 +1,5 @@
 from django.db.models import Model, TextField, UniqueConstraint, ForeignKey, DO_NOTHING, \
-    ImageField, FloatField, CheckConstraint, Q
+    ImageField, FloatField, CheckConstraint, Q, BigIntegerField, UUIDField, CharField
 
 
 class Place(Model):
@@ -75,3 +75,57 @@ class PlaceTagged(Model):
                 name="placetagged_place_tag_key"
             )
         ]
+
+
+class Editor(Model):
+    """Representa un usuario que contribuye con la información que muestra el bot.
+
+    telegram_id:
+    El id único de la cuenta de usuario de telegram
+    """
+    telegram_id = BigIntegerField(unique=True)
+
+
+class Edition(Model):
+    """Representa una sugerencia de edición a un lugar determinado.
+    Por ejemplo editar el nombre del lugar o su contacto.
+
+    editor:
+    El usuario que realiza la edición.
+
+    unique_id:
+    ID que permite identificar esta sugerencia entre todas las demás.
+
+    field_type:
+    El tipo de edición, puede ser name, contact, location, schedule, tag o photo.
+
+    place:
+    El lugar al que se aplica la edición.
+
+    text:
+    El contanido de la edición para su revisión y posterior inserción o descarte.
+
+    """
+    editor = ForeignKey(Editor, DO_NOTHING)
+    unique_id = UUIDField()
+    field_type = CharField(max_length=100)
+    place = ForeignKey(Place, DO_NOTHING)
+    text = CharField(max_length=5000)
+
+
+class Addition(Model):
+    """Representa una sugerencia de adición de algún lugar.
+
+    editor:
+    El usuario que realiza la edición.
+
+    text:
+    La información del lugar para su revisión y posterior inserción o descarte.
+
+    photo:
+    El id (file_id) único de telegram de la foto para su posterior almacenamiento.
+
+    """
+    editor = ForeignKey(Editor, DO_NOTHING)
+    text = CharField(max_length=5000)
+    photo = CharField(max_length=1000, null=True)
