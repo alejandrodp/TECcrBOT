e0cab866d1acd063c1e143b9348693328f2fad72
Fabian Montero <fabian@cluster451.org>
2022-01-31 21:44:58 -0600

    Añade modelos a directory

---
+++ directory/models.py
@@ -0,0 +1,133 @@
+from django.db.models import Model, TextField, ForeignKey, DO_NOTHING, UniqueConstraint
+
+
+class Person(Model):
+    """Representa una persona en el directorio del bot.
+
+    name:
+    Nombre de la persona.
+
+    surname:
+    Apellidos de la persona.
+
+    email:
+    Correo de la persona.
+
+    phone:
+    Número de la persona.
+
+    href:
+    Url al perfil de la persona en la página del tec.
+    """
+
+    name = TextField(max_length=100)
+    surname = TextField(max_length=100)
+    email = TextField(max_length=500)
+    phone = TextField(max_length=100)
+    href = TextField(max_length=1000)
+
+
+class Ty(Model):
+    """Se refiere al "tipo" o "puesto" que tiene una persona dentro de un departamento.
+
+    Por ejemplo, "coordinador", "directora", "presidente" etc.
+
+    name:
+    Nombre del tipo.
+    """
+
+    name = TextField(max_length=100, unique=True)
+
+
+class Function(Model):
+    """Se refiere a la función que cumple una persona en un departamento.
+
+    Por ejemplo, "profesor", "investigador, etc.
+
+    name:
+    Nombre de la función.
+    """
+
+    name = TextField(max_length=100, unique=True)
+
+
+class Location(Model):
+    """Ubicaciones de las diferentes unidades.
+
+    Por lo general, sirve para diferenciar unidades que son iguales pero están en diferentes campus.
+    Por ejemplo, para diferenciar entre la Escuela de Matemática de Cartago y la de San José.
+
+    name:
+    Nombre del departamento.
+    """
+
+    name = TextField(max_length=100, unique=True)
+
+
+class UnitTy(Model):
+    """Tipos de unidades.
+
+    Básicamente sirve para diferenciar entre escuelas/áreas académicas y los demás departamentos.
+
+    name:
+    Nombre del tipo de la unidad.
+    """
+
+    name = TextField(max_length=100)
+
+
+class Unit(Model):
+    """Departamentos, unidades, escuelas, etc.
+
+    name:
+    Nombre de la unidad.
+
+    url:
+    Link a la página del TEC de la unidad.
+
+    location:
+    Ubicación de la unidad.
+    """
+
+    name = TextField(max_length=100, unique=True)
+    url = TextField(max_length=1000)
+    location = ForeignKey(Location, DO_NOTHING)
+    ty = ForeignKey(UnitTy, DO_NOTHING)
+
+
+class Role(Model):
+    """Rol que cumple la persona en su respectivo departamento.
+
+    Está compuesto por su tipo y su función. Ver documentación de tipo y función.
+
+    person:
+    Persona que ejerce el rol.
+
+    department:
+    Departamento en el cual la persona ejerce el rol.
+
+    type:
+    Tipo de rol.
+
+    function:
+    Función del rol.
+    """
+
+    person = ForeignKey(Person, DO_NOTHING)
+    department = ForeignKey(Unit, DO_NOTHING)
+    ty = ForeignKey(Ty, DO_NOTHING)
+    function = ForeignKey(Function, DO_NOTHING)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['person', 'department', 'ty', 'function'],
+                name='role_person_department_ty_function_key'
+            )
+        ]
+
+
+
+
+
+
