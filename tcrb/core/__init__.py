1f6085e18ab44471d131142b0d8f6758356979e1
Alejandro Díaz <alejandrodp@protonmail.com>
2022-02-28 11:56:05 -0600

    Limpieza

--- tcrb/core/__init__.py
+++ tcrb/core/__init__.py
@@ -1,2 +1,3 @@
+# TODO: Eliminar esto cuando se llegue al checkpoint establecido para Abril
 beta_msg = "<b>Este bot aún está en desarrollo, algunas consultas pueden no funcionar. " \
            "Estaremos monitoreando sus consultas para mejorar la búsqueda.</b>"
