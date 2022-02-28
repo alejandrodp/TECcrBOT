4a14e42f9952783da7bce1ecc791143ebdeec57e
Alejandro Díaz <alejandrodp@protonmail.com>
2022-02-27 21:44:19 -0600

    Refactoriza constantes de places

--- tcrb/apps/places/settings.py
+++ tcrb/apps/places/settings.py
@@ -1,6 +1,7 @@
-from .constants import PAGE_TY
 from .models import Place
 from .util import show_place
 from ...core.apps.pages import PageTy
 
-PLACE_PAGES = PageTy(ty=PAGE_TY, model=Place, desc='Ubicaciones', build=show_place)
+PLACES_DESC = "Ubicaciones \U0001f4cd"
+
+PLACE_PAGES = PageTy(ty=4, model=Place, desc=PLACES_DESC, build=show_place)
