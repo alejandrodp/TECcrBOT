41ea376083f88a80efde5a5c42cec3feb3a6b521
Alejandro Díaz <alejandrodp@protonmail.com>
2022-02-27 21:54:16 -0600

    Elimina archivo de constantes de directory innecesario

--- tcrb/apps/directory/settings.py
+++ tcrb/apps/directory/settings.py
@@ -1,11 +1,10 @@
-from .constants import PEOPLE_TY, DEPT_TY, LOC_TY
 from .models import Person, Unit, Location
 from .util import index_people, people_builder, depts_page_builder, loc_builder
 from ...core.apps.pages import PageTy
 
-PEOPLE_PAGES = PageTy(ty=PEOPLE_TY, model=Person,
+PEOPLE_PAGES = PageTy(ty=1, model=Person,
                       desc='personas', index=index_people, build=people_builder)
 
-DEPT_PAGES = PageTy(ty=DEPT_TY, model=Unit, desc='departamentos', build=depts_page_builder)
+DEPT_PAGES = PageTy(ty=2, model=Unit, desc='departamentos', build=depts_page_builder)
 
-LOC_PAGES = PageTy(ty=LOC_TY, model=Location, desc='campuses', build=loc_builder)
+LOC_PAGES = PageTy(ty=3, model=Location, desc='campuses', build=loc_builder)
