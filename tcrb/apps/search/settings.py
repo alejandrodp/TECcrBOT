745c72102829ccac60d833dbe958d9a8dfcf7de1
Alejandro Díaz <alejandrodp@protonmail.com>
2022-02-28 02:22:35 -0600

    Agrega filtro para evitar búsqueda de texto de mensajes editados

--- tcrb/apps/search/settings.py
+++ tcrb/apps/search/settings.py
@@ -4,5 +4,8 @@ from tcrb.apps.config.handlers import HandlerConfig, MessageHandler
 from tcrb.apps.search.handlers import search_handler
 
 SEARCH_HANDLERS = HandlerConfig([
-    MessageHandler(Filters.text & ~Filters.command, search_handler)
+    MessageHandler(Filters.text &
+                   ~Filters.command &
+                   ~Filters.update.edited_message &
+                   ~Filters.update.edited_channel_post, search_handler)
 ])
