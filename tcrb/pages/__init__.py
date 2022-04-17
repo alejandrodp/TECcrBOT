1cfd6e586a247fe4879ccdd781b269365790be09
Alejandro Díaz <alejandrodp@protonmail.com>
2022-04-16 23:11:18 -0600

    Repara typo en pages

--- tcrb/pages/__init__.py
+++ tcrb/pages/__init__.py
@@ -1,7 +1,7 @@
-from buttons import build_show_page_button
-from util import PageTy
-from util import show_page
-from settings import PAGE_HANDLER_CONFIGS
+from .buttons import build_show_page_button
+from .util import PageTy
+from .util import show_page
+from .settings import PAGE_HANDLER_CONFIGS
 
 
 __all__ = [
--- tcrb/pages/settings.py
+++ tcrb/pages/settings.py
@@ -1,6 +1,6 @@
 import tcrb.core.handlers
-import handlers
-import buttons
+from . import handlers
+from . import buttons
 
 PAGE_HANDLER_CONFIGS = tcrb.core.handlers.HandlerConfig([
     tcrb.core.handlers.CallbackQueryHandler(buttons.show_page_button, handlers.show_page_handler)
