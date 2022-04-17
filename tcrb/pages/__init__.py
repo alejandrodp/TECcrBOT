17bdcd92984aa2db85d556c197ca76b342af357e
Alejandro Díaz <alejandrodp@protonmail.com>
2022-04-16 22:42:04 -0600

    Añade __init__ a módulos pages

---
+++ tcrb/pages/__init__.py
@@ -0,0 +1,11 @@
+from buttons import build_show_page_button
+from util import PageTy
+from util import show_page
+from settings import PAGE_HANDLER_CONFIGS
+
+
+__all__ = [
+    "build_show_page_button",
+    "PageTy",
+    "show_page"
+]
