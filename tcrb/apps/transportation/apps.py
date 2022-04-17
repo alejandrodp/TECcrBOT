c2d91af684f8ee4c42cb241704ad1158e032303b
Alejandro Díaz <alejandrodp@protonmail.com>
2022-04-17 02:53:02 -0600

    Limpia módulo de servicios de transporte

--- tcrb/apps/transportation/apps.py
+++ tcrb/apps/transportation/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class TransportationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tcrb.apps.transportation'
-    verbose_name = "Servicios de transporte \U0001f68c"
