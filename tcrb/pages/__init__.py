efef6ca265ce5c89321448f1e9efc8229ecbc4f5
Alejandro Díaz <alejandrodp@protonmail.com>
2022-04-17 03:32:39 -0600

    Agrega all_pages al __init__ del módulo pages

--- tcrb/pages/__init__.py
+++ tcrb/pages/__init__.py
@@ -2,10 +2,12 @@ from .util import PageTy
 from .util import show_page
 from .util import build_show_page_button
 from .settings import PAGE_HANDLER_CONFIGS
+from .config import all_pages
 
 
 __all__ = [
     "build_show_page_button",
     "PageTy",
-    "show_page"
+    "show_page",
+    "all_pages",
 ]
